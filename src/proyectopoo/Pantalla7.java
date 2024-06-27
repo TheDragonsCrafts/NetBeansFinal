2cd9800d1d17ec19b0e7de3fbd65c25d69f9633a
LAB 314 PC xx <LAB 314 PC xx@DESKTOP-277Q885>
2024-06-26 20:09:57 -0600

    alguien se comio mi SANDWIIIIIIIICH

--- src/proyectopoo/Pantalla7.java
+++ src/proyectopoo/Pantalla7.java
@@ -35,7 +35,7 @@ public class Pantalla7 extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(255, 0, 0));
+        jPanel1.setBackground(new java.awt.Color(0, 153, 153));
 
         TablaCorte.setFont(new java.awt.Font("Segoe UI Semibold", 3, 18)); // NOI18N
         TablaCorte.setModel(new javax.swing.table.DefaultTableModel(
